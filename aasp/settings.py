842fb7e2309b2ecd8d5631cb7ff8d75ab8aa36d3
Jun Wei <58636401+leejunweisg@users.noreply.github.com>
2022-09-01 17:20:52 +0800

    Updated docker configs

--- aasp/settings.py
+++ aasp/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'aasp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("POSTGRES_DB"),
-        'USER': os.environ.get("POSTGRES_USER"),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
-        'HOST': os.environ.get("POSTGRES_HOST"),
-        'PORT': os.environ.get("POSTGRES_PORT"),
+        'NAME': os.environ.get("AASP_POSTGRES_DB"),
+        'USER': os.environ.get("AASP_POSTGRES_USER"),
+        'PASSWORD': os.environ.get("AASP_POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("AASP_POSTGRES_HOST"),
+        'PORT': os.environ.get("AASP_POSTGRES_PORT"),
     },
 }
 

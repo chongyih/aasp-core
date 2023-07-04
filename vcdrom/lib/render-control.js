0f919702fd012e03ef093b2725773abf91f29864
Chua Chong Yih <chuachongyih@gmail.com>
2023-07-04 20:42:38 +0800

    added icons to control menu

--- vcdrom/lib/render-control.js
+++ vcdrom/lib/render-control.js
@@ -1,15 +1,15 @@
 const stringify = require("onml/stringify");
 
 const renderControl = () => {
-    const zoomIn = ['button', {id: 'zoom-in'}, '+'];
-    const zoomOut = ['button', {id: 'zoom-out'}, '-'];
-    const zoomReset = ['button', {id: 'zoom-reset'}, '0'];
-    const jumpBeginning = ['button', {id: 'jump-beginning'}, 'Start'];
-    const jumpEnd = ['button', {id: 'jump-end'}, 'End'];
-    const shiftLeft = ['button', {id: 'shift-left'}, '<'];
-    const shiftRight = ['button', {id: 'shift-right'}, '>'];
-
-    const controls = ['div', {class: 'controls'}, zoomIn, zoomOut, zoomReset, jumpBeginning, jumpEnd, shiftLeft, shiftRight];
+    const zoomIn = ['button', {id: 'zoom-in', class: 'icon-button'}, '<i class="fa fa-search-plus icon-style"></i>'];
+    const zoomOut = ['button', {id: 'zoom-out', class: 'icon-button'}, '<i class="fa fa-search-minus icon-style"></i>'];
+    const zoomReset = ['button', {id: 'zoom-reset', class: 'icon-button'}, '<i class="fa fa-refresh icon-style"></i>'];
+    const jumpBeginning = ['button', {id: 'jump-beginning', class: 'icon-button'}, '<i class="fa fa-step-backward icon-style"></i>'];
+    const shiftLeft = ['button', {id: 'shift-left', class: 'icon-button'}, '<i class="fa fa-arrow-left icon-style"></i>'];
+    const shiftRight = ['button', {id: 'shift-right', class: 'icon-button'}, '<i class="fa fa-arrow-right icon-style"></i>'];
+    const jumpEnd = ['button', {id: 'jump-end', class: 'icon-button'}, '<i class="fa fa-step-forward icon-style"></i>'];
+    
+    const controls = ['div', {class: 'controls'}, zoomIn, zoomOut, zoomReset, jumpBeginning, shiftLeft, shiftRight, jumpEnd];
 
     return stringify(controls)
 }
